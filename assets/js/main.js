1c0f20e76477e1a53cfa65f583cc23d4b1582a23
artur_arczi <artjuzwik93@gmail.com>
2019-07-21 19:02:00 +0200

    i create createlaout function, its mostly based on previous version, i create tags using createelement instead innerhtml

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,7 +123,7 @@ let controls = (searchObj,data) => {
                 }else{
                     for(let props in searchObj){
                         if((searchObj[props] == true) && data[i].category == props){
-                            console.log(data[i])
+                            createLayout(data[i])
                         };
                     }; 
                 };
@@ -132,6 +132,37 @@ let controls = (searchObj,data) => {
     };
     performSearch(searchObj,data);
     document.querySelector('button.search').addEventListener('click',() => {performSearch(searchObj,data)});
+
+    function createLayout(data){
+
+        let mainOutput = document.querySelector('.output');
+
+        let singleReport = document.createElement('DIV');
+            singleReport.classList.add('report');
+
+        let metaBox = document.createElement('DIV');
+            metaBox.classList.add('meta');
+        let innerMetaP1 = document.createElement('P');
+            innerMetaP1.classList.add('time');
+
+            innerMetaP1.innerHTML = 
+                ((data.date.getDay() <= 9) ? ('0' + data.date.getDay()) : data.date.getDay()) + '.' 
+                + ((data.date.getMonth() <= 9) ? ('0' + data.date.getMonth()) : data.date.getMonth()) + '.' 
+                + data.date.getFullYear() + 
+                '<br/> ' + data.date.getHours() + '.' + ((data.date.getMinutes() <= 9) ? ('0' + data.date.getMinutes()) : data.date.getMinutes()); 
+            
+            metaBox.appendChild(innerMetaP1)
+        
+        let innerMetaP2 = document.createElement('P');
+            innerMetaP2.classList.add('category');
+
+            innerMetaP2.innerHTML = 'Raporty ' + data.category.toLowerCase()
+            
+            metaBox.appendChild(innerMetaP2);
+
+        singleReport.appendChild(metaBox);
+        mainOutput.appendChild(singleReport);
+    };
 };
 
 
