e96eb07f8d5d812db4642ec1c57cd7391574408a
artur_arczi <artjuzwik93@gmail.com>
2019-07-21 19:30:05 +0200

    create external file for createLayout function
    and import them

---
+++ assets/js/createLayout.js
@@ -0,0 +1,45 @@
+function createLayout(data){
+
+    let mainOutput = document.querySelector('.output');
+
+    let singleReport = document.createElement('DIV');
+        singleReport.classList.add('report');
+
+    let metaBox = document.createElement('DIV');
+        metaBox.classList.add('meta');
+
+        singleReport.appendChild(metaBox);
+
+    let innerMetaP1 = document.createElement('P');
+        innerMetaP1.classList.add('time');
+
+        innerMetaP1.innerHTML = 
+            ((data.date.getDay() <= 9) ? ('0' + data.date.getDay()) : data.date.getDay()) + '.' 
+            + ((data.date.getMonth() <= 9) ? ('0' + data.date.getMonth()) : data.date.getMonth()) + '.' 
+            + data.date.getFullYear() + 
+            '<br/> ' + data.date.getHours() + '.' + ((data.date.getMinutes() <= 9) ? ('0' + data.date.getMinutes()) : data.date.getMinutes()); 
+        
+        metaBox.appendChild(innerMetaP1)
+    
+    let innerMetaP2 = document.createElement('P');
+        innerMetaP2.classList.add('category');
+
+        innerMetaP2.innerHTML = 'Raporty ' + data.category.toLowerCase()
+        
+        metaBox.appendChild(innerMetaP2);
+
+    let reportDetails = document.createElement('DIV');
+        reportDetails.classList.add('holder');
+
+    let reportDetailsTitle = document.createElement('H2');
+        reportDetailsTitle.innerHTML = data.title;
+
+        reportDetails.appendChild(reportDetailsTitle);
+
+    let reportDescription = document.createElement('P');    
+        reportDescription.innerHTML = data.description;    
+
+    
+    mainOutput.appendChild(singleReport);
+};
+export default createLayout;
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
+import createLayout from './createLayout.js';
 'use strict'
+
+
+
 let selectOptionsContainer = document.querySelector('.filter .search .select-year-container .years');
 let categoriesTabsContainer = document.querySelector('.filter .search .categories');
 
@@ -133,46 +137,6 @@ let controls = (searchObj,data) => {
     performSearch(searchObj,data);
     document.querySelector('button.search').addEventListener('click',() => {performSearch(searchObj,data)});
 
-    function createLayout(data){
-
-        let mainOutput = document.querySelector('.output');
-
-        let singleReport = document.createElement('DIV');
-            singleReport.classList.add('report');
-
-        let metaBox = document.createElement('DIV');
-            metaBox.classList.add('meta');
-
-            singleReport.appendChild(metaBox);
-
-        let innerMetaP1 = document.createElement('P');
-            innerMetaP1.classList.add('time');
-
-            innerMetaP1.innerHTML = 
-                ((data.date.getDay() <= 9) ? ('0' + data.date.getDay()) : data.date.getDay()) + '.' 
-                + ((data.date.getMonth() <= 9) ? ('0' + data.date.getMonth()) : data.date.getMonth()) + '.' 
-                + data.date.getFullYear() + 
-                '<br/> ' + data.date.getHours() + '.' + ((data.date.getMinutes() <= 9) ? ('0' + data.date.getMinutes()) : data.date.getMinutes()); 
-            
-            metaBox.appendChild(innerMetaP1)
-        
-        let innerMetaP2 = document.createElement('P');
-            innerMetaP2.classList.add('category');
-
-            innerMetaP2.innerHTML = 'Raporty ' + data.category.toLowerCase()
-            
-            metaBox.appendChild(innerMetaP2);
-
-        let reportDetails = document.createElement('DIV');
-            reportDetails.classList.add('holder');
-
-        let reportDetailsTitle = document.createElement('H2');
-            reportDetailsTitle.innerHTML = data.title
-                
-
-        
-        mainOutput.appendChild(singleReport);
-    };
 };
 
 
