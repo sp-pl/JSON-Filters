afdcdbb2b11b85514b92b4e58e6bd1dd97193c68
artur_arczi <artjuzwik93@gmail.com>
2019-07-21 18:24:21 +0200

    beautyfing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 'use strict'
-//datalink: http://localhost:3000/assets/data/data.json
 let selectOptionsContainer = document.querySelector('.filter .search .select-year-container .years');
 let categoriesTabsContainer = document.querySelector('.filter .search .categories');
 
@@ -32,11 +31,9 @@ let initApp = (data) => {
 };
 
 let formatData = (data) => {
-    //sort data from lowest to highest
     data.sort((a,b) => {
         return a.date - b.date
     });
-    //turn posix to date
     for(let i = 0; i < data.length; i++){
          data[i].date = new Date(data[i].date)
     };
@@ -44,16 +41,12 @@ let formatData = (data) => {
 };
 
 let createYearOptions = (data,propName) => {
-    //get every year
     let allYears = [];
     for(let i = 0; i < data.length; i++){
         allYears.push(data[i][propName].getFullYear());
     };
     
-    //get single years
     let singleYears = allYears.filter((v,index,self) => self.indexOf(v) === index);
-
-    //create and append options
     for(let i = 0; i < singleYears.length; i++){
         let newOption = document.createElement('OPTION');
         newOption.textContent = singleYears[i];
@@ -63,15 +56,12 @@ let createYearOptions = (data,propName) => {
 };
 
 let createCategoriesTabs = (data,propName) => {
-    //get every category
     let categories = [];
     for(let i = 0; i < data.length; i++){
         categories.push(data[i][propName])
     }
-    //get single categories from every category
-    let singleCategories = categories.filter((v,index,self) => self.indexOf(v) === index );
 
-    //create and append tabs
+    let singleCategories = categories.filter((v,index,self) => self.indexOf(v) === index );
     for(let i = 0; i < singleCategories.length; i++){
         let newButton = document.createElement('BUTTON');
         newButton.textContent = singleCategories[i];
@@ -80,8 +70,6 @@ let createCategoriesTabs = (data,propName) => {
     };
     return singleCategories;
 };
-// createCategoriesTabs(formatData(arr),'category');
-
 
 let searchObjInit = (categories) => {
     let searchObj = {
@@ -107,7 +95,6 @@ let controls = (searchObj,data) => {
 
     function categoryTabsBool(evt){
         let categoriesAllTabs = document.querySelectorAll('.filter .search .categories button');
-
         if(evt.target.dataset.category === 'all' && !evt.target.classList.contains('active')){
             evt.target.classList.add('active');
             searchObj.all = true;
@@ -128,9 +115,7 @@ let controls = (searchObj,data) => {
     };
     categoriesTabsContainer.addEventListener('click',categoryTabsBool);
     
-
     function performSearch(searchObj,data){
-
         for(let i = 0; i<data.length; i++){
             if(data[i].date.getFullYear() == searchObj.year){
                 if(searchObj.all === true){
@@ -139,14 +124,14 @@ let controls = (searchObj,data) => {
                     for(let props in searchObj){
                         if((searchObj[props] == true) && data[i].category == props){
                             console.log(data[i])
-                        }
-                    } 
-                }
+                        };
+                    }; 
+                };
             };
-        }
-    }
+        };
+    };
+    performSearch(searchObj,data);
     document.querySelector('button.search').addEventListener('click',() => {performSearch(searchObj,data)});
-
 };
 
 
