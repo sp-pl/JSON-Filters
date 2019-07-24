3be8a2ef089861be1e6affec7cd40f59033997e9
artur_arczi <artjuzwik93@gmail.com>
2019-07-24 14:13:16 +0200

    remove console log

--- assets/js/appendReport.js
+++ assets/js/appendReport.js
@@ -1,7 +1,5 @@
 function createLayout(data){
-    console.log(data)
     let mainOutput = document.querySelector('.output');
-
     let singleReport = document.createElement('DIV');
         singleReport.classList.add('report');
 
