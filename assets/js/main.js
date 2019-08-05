d4a4b1ccb3ade50239fde64904c6e56c7a663b0c
artur_arczi <artjuzwik93@gmail.com>
2019-08-05 11:30:56 +0200

    lol

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,157 +1,154 @@
 import appendReport from './appendReport.js';
 
-(function(){
-'use strict'
+let selectOptionsContainer = document.querySelector('.filter .search .select-year-container .years');
+let categoriesTabsContainer = document.querySelector('.filter .search .categories');
+let mainOutput = document.querySelector('.output'); 
+
+let fetchData = (link) => {
+    fetch(link, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json'
+        }
+    })
+    .then(response => {
+        if(response.ok){
+           return response.json();
+        }else{
+            throw new Error('server not')
+        }
+    })
+    .then(json => {
+        initApp(json)
+    })
+};
+fetchData('http://localhost:3000/assets/data/data.json');
+
+let initApp = (data) => {
+    formatData(data);
+    createYearOptions(data,'date');
+    let uniqueCategories = createCategoriesTabs(data,'category');
+    let searchObjectFirst = searchObjInit(uniqueCategories);
+    controls(searchObjectFirst, data);
+};
+
+let formatData = (data) => {
+    data.sort((a,b) => {
+        return a.date - b.date
+    });
+    for(let i = 0; i < data.length; i++){
+         data[i].date = new Date(data[i].date)
+    };
+    return data;
+};
+
+let createYearOptions = (data,propName) => {
+    let allYears = [];
+    for(let i = 0; i < data.length; i++){
+        allYears.push(data[i][propName].getFullYear());
+    };
     
-    let selectOptionsContainer = document.querySelector('.filter .search .select-year-container .years');
-    let categoriesTabsContainer = document.querySelector('.filter .search .categories');
-    let mainOutput = document.querySelector('.output'); 
+    let singleYears = allYears.filter((v,index,self) => self.indexOf(v) === index);
+    for(let i = 0; i < singleYears.length; i++){
+        let newOption = document.createElement('OPTION');
+        newOption.textContent = singleYears[i];
+        selectOptionsContainer.appendChild(newOption);
+    }
+};
 
-    let fetchData = (link) => {
-        fetch(link, {
-            method:'GET',
-            headers: {
-                Accept: 'application/json'
-            }
-        })
-        .then(response => {
-            if(response.ok){
-               return response.json();
-            }else{
-                throw new Error('server not')
-            }
-        })
-        .then(json => {
-            initApp(json)
-        })
+let createCategoriesTabs = (data,propName) => {
+    let categories = [];
+    for(let i = 0; i < data.length; i++){
+        categories.push(data[i][propName])
+    }
+
+    let singleCategories = categories.filter((v,index,self) => self.indexOf(v) === index );
+    for(let i = 0; i < singleCategories.length; i++){
+        let newButton = document.createElement('BUTTON');
+        newButton.textContent = singleCategories[i];
+        newButton.dataset.category = singleCategories[i];
+        categoriesTabsContainer.appendChild(newButton);
     };
-    fetchData('http://localhost:3000/assets/data/data.json');
+    return singleCategories;
+};
 
-    let initApp = (data) => {
-        formatData(data);
-        createYearOptions(data,'date');
-        let uniqueCategories = createCategoriesTabs(data,'category');
-        let searchObjectFirst = searchObjInit(uniqueCategories);
-        controls(searchObjectFirst, data);
+let searchObjInit = (categories) => {
+    let searchObj = {
+        year: '',
+        all: '' 
     };
-
-    let formatData = (data) => {
-        data.sort((a,b) => {
-            return a.date - b.date
-        });
-        for(let i = 0; i < data.length; i++){
-             data[i].date = new Date(data[i].date)
+    searchObj.year = selectOptionsContainer.value;
+    searchObj.all = true;
+    if(categories){
+        for(let i = 0; i<categories.length; i++){
+            searchObj[categories[i]] = false;
         };
-        return data;
     };
+    return searchObj;
+};
 
-    let createYearOptions = (data,propName) => {
-        let allYears = [];
-        for(let i = 0; i < data.length; i++){
-            allYears.push(data[i][propName].getFullYear());
-        };
-        
-        let singleYears = allYears.filter((v,index,self) => self.indexOf(v) === index);
-        for(let i = 0; i < singleYears.length; i++){
-            let newOption = document.createElement('OPTION');
-            newOption.textContent = singleYears[i];
-            selectOptionsContainer.appendChild(newOption);
-        }
-    };
+let controls = (searchObj,data) => {
 
-    let createCategoriesTabs = (data,propName) => {
-        let categories = [];
-        for(let i = 0; i < data.length; i++){
-            categories.push(data[i][propName])
-        }
+    function selectOptionsSet(evt){
+        searchObj.year = evt.target.value;
+    }
+    selectOptionsContainer.addEventListener('change',selectOptionsSet);
 
-        let singleCategories = categories.filter((v,index,self) => self.indexOf(v) === index );
-        for(let i = 0; i < singleCategories.length; i++){
-            let newButton = document.createElement('BUTTON');
-            newButton.textContent = singleCategories[i];
-            newButton.dataset.category = singleCategories[i];
-            categoriesTabsContainer.appendChild(newButton);
-        };
-        return singleCategories;
-    };
-
-    let searchObjInit = (categories) => {
-        let searchObj = {
-            year: '',
-            all: '' 
-        };
-        searchObj.year = selectOptionsContainer.value;
-        searchObj.all = true;
-        if(categories){
-            for(let i = 0; i<categories.length; i++){
-                searchObj[categories[i]] = false;
+    function categoryTabsBool(evt){
+        evt.preventDefault();
+        let categoriesAllTabs = document.querySelectorAll('.filter .search .categories button');
+        if(evt.target.dataset.category === 'all' && !evt.target.classList.contains('active')){
+            evt.target.classList.add('active');
+            searchObj.all = true;
+            for(let i = 1; i < categoriesAllTabs.length; i++){
+                categoriesAllTabs[i].classList.remove('active');
+                searchObj[categoriesAllTabs[i].dataset.category] = false;
             };
-        };
-        return searchObj;
-    };
-
-    let controls = (searchObj,data) => {
-
-        function selectOptionsSet(evt){
-            searchObj.year = evt.target.value;
         }
-        selectOptionsContainer.addEventListener('change',selectOptionsSet);
-
-        function categoryTabsBool(evt){
+        if((evt.target.dataset.category !== 'all' && !evt.target.classList.contains('active')) && evt.target.tagName === 'BUTTON'){
+            categoriesAllTabs[0].classList.remove('active');
+            searchObj.all = false;
+            evt.target.classList.add('active');
+            searchObj[evt.target.dataset.category] = true;
+        }else if((evt.target.dataset.category !== 'all' && evt.target.classList.contains('active')) && evt.target.tagName === 'BUTTON'){
+            evt.target.classList.remove('active');
+            searchObj[evt.target.dataset.category] = false;
+        };
+    };
+    categoriesTabsContainer.addEventListener('click',categoryTabsBool);
+    
+    function performSearch(searchObj,data,evt){
+        if(evt){
             evt.preventDefault();
-            let categoriesAllTabs = document.querySelectorAll('.filter .search .categories button');
-            if(evt.target.dataset.category === 'all' && !evt.target.classList.contains('active')){
-                evt.target.classList.add('active');
-                searchObj.all = true;
-                for(let i = 1; i < categoriesAllTabs.length; i++){
-                    categoriesAllTabs[i].classList.remove('active');
-                    searchObj[categoriesAllTabs[i].dataset.category] = false;
-                };
-            }
-            if((evt.target.dataset.category !== 'all' && !evt.target.classList.contains('active')) && evt.target.tagName === 'BUTTON'){
-                categoriesAllTabs[0].classList.remove('active');
-                searchObj.all = false;
-                evt.target.classList.add('active');
-                searchObj[evt.target.dataset.category] = true;
-            }else if((evt.target.dataset.category !== 'all' && evt.target.classList.contains('active')) && evt.target.tagName === 'BUTTON'){
-                evt.target.classList.remove('active');
-                searchObj[evt.target.dataset.category] = false;
-            };
-        };
-        categoriesTabsContainer.addEventListener('click',categoryTabsBool);
-        
-        function performSearch(searchObj,data,evt){
-            if(evt){
-                evt.preventDefault();
-            }
-            mainOutput.innerHTML = "";
-            for(let i = 0; i<data.length; i++){
-                if(data[i].date.getFullYear() == searchObj.year){
-                    if(searchObj.all === true){
-                        appendReport(data[i]);
-                    }else{
-                        for(let props in searchObj){
-                            if((searchObj[props] == true) && data[i].category == props){
-                                appendReport(data[i])
-                            };
-                        }; 
-                    };
-                };
-            };
-        };
-        performSearch(searchObj,data);
-        document.querySelector('button.search').addEventListener('click',(evt) => {performSearch(searchObj,data,evt)});
-
-        function textFilter(evt){
-            let allVisibleReports = document.querySelectorAll('.report');
-            for(let i = 0; i < allVisibleReports.length; i++){
-                if(!allVisibleReports[i].innerText.toLowerCase().includes(evt.target.value.toLowerCase())){
-                    allVisibleReports[i].style.display = 'none';
+        }
+        mainOutput.innerHTML = "";
+        for(let i = 0; i<data.length; i++){
+            if(data[i].date.getFullYear() == searchObj.year){
+                if(searchObj.all === true){
+                    appendReport(data[i]);
                 }else{
-                    allVisibleReports[i].style.display = 'flex';
-                }
-            }
+                    for(let props in searchObj){
+                        if((searchObj[props] == true) && data[i].category == props){
+                            appendReport(data[i])
+                        };
+                    }; 
+                };
+            };
         };
-        document.querySelector('.filter .search .input-container input').addEventListener('input',(evt) => {textFilter(evt)})        
     };
-}());
+    performSearch(searchObj,data);
+    document.querySelector('button.search').addEventListener('click',(evt) => {performSearch(searchObj,data,evt)});
+
+    function textFilter(evt){
+        let allVisibleReports = document.querySelectorAll('.report');
+        for(let i = 0; i < allVisibleReports.length; i++){
+            if(!allVisibleReports[i].innerText.toLowerCase().includes(evt.target.value.toLowerCase())){
+                allVisibleReports[i].style.display = 'none';
+            }else{
+                allVisibleReports[i].style.display = 'flex';
+            }
+        }
+    };
+    document.querySelector('.filter .search .input-container input').addEventListener('input',(evt) => {textFilter(evt)})        
+};
+
