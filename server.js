872bd3dd4e4c9a3a112c5a3283a9cb03b2340189
artur_arczi <artjuzwik93@gmail.com>
2019-08-05 11:22:30 +0200

    beautifing

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const path = require('path');
-var sassMiddleware = require('node-sass-middleware');
+const path = require('path')
+const sassMiddleware = require('node-sass-middleware')
 
 app.use(
   sassMiddleware({
-    src:__dirname ,
-    dest: __dirname ,
+    src:__dirname,
+    dest: __dirname,
     debug: true,
     outputStyle: 'compressed'
   })
 );
 
-app.use(express.static('assets'));
+app.use(express.static('assets'))
 app.use("/assets", express.static(__dirname + '/assets'));
-app.get('/',function(req,res) {
-res.sendFile('index.html', {
-    root: path.join(__dirname, './')
-})
+app.get('/', function(req,res) {
+	res.sendFile('index.html', {
+	    root: path.join(__dirname, './')
+	})
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
